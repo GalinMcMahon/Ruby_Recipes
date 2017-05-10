202e519db2afc3920716d7c0fd0c31bea325a2a8
Galin McMahon and Mohamed Warsame <galin@>
2017-05-10 16:45:09 -0700

    added initial sinatra and routing, some css

--- app.rb
+++ app.rb
@@ -3,3 +3,26 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  erb(:index)
+end
+
+get("/ingredients") do
+  @ingredients = Ingredient.all()
+  erb(:ingredients)
+end
+
+get("/recipes") do
+  @recipes = Recipe.all()
+  erb(:recipes)
+end
+
+get("/tags") do
+  @tags = Tag.all()
+  erb(:tags)
+end
+
+get("/recipe_form") do
+  erb(:recipe_form)
+end
