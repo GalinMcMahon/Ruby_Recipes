5666ff1e39e0a3fc70bc089208840b786f360965
Galin McMahon and Mohamed Warsame <mohamed@>
2017-05-11 09:45:53 -0700

    add tag erb, add instuction colmn to recipe table

--- app.rb
+++ app.rb
@@ -24,5 +24,32 @@ get("/tags") do
 end
 
 get("/recipe_form") do
+  @tags = Tag.all
   erb(:recipe_form)
+end
+
+post('/tags') do
+  new_tag = (params.fetch("tag"))
+  tag = Tag.create({tag: new_tag})
+  @tags = Tag.all
+  redirect '/tags'
+end
+
+get('/tags/:id') do
+  @tag = Tag.find(params.fetch("id").to_i)
+  erb(:tag)
+end
+
+patch('/rename_tag/:id') do
+  tag = params.fetch("tag")
+  @tag = Tag.find(params.fetch("id").to_i())
+  @tag.update({tag: tag})
+  erb(:tag)
+end
+
+delete('/delete_tag/:id') do
+  @tag = Tag.find(params.fetch('id').to_i)
+  @tag.delete
+  @tags = Tag.all
+  erb(:tags)
 end
